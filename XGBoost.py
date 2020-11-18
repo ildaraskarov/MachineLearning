9688e007a4347cfd5f16f8f9b1516bf11bc1d15e
ildar.askarov.0405@gmail.com <ildar.askarov.0405@gmsil.com>
2020-11-18 11:16:16 +0100

    define a function with data preprocessing, adding data folder where all data will be stored

--- XGBoost.py
+++ XGBoost.py
@@ -6,43 +6,27 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import balanced_accuracy_score, roc_auc_score, make_scorer
 from sklearn.metrics import confusion_matrix, plot_confusion_matrix
 import matplotlib.pyplot as plt
+import os
+import glob
+import pathlib
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
-df = pd.read_csv('Telco_customer_churn.csv')
+def preprocess_Telco_customer_churn_data(filename):
 
-df.drop('customerID', axis=1, inplace=True)
+    core_path = os.getcwd()
+    path_to_file = os.path.join(core_path, filename)
 
-print(df.head())
-
-print('===================================')
-print(len(df.loc[df['TotalCharges'] == ' ']))
-
-df.loc[(df['TotalCharges'] == ' '), 'TotalCharges'] = 0
-
-print('===================================')
-print(df.loc[df['tenure'] == 0])
-
-df['TotalCharges'] = pd.to_numeric(df['TotalCharges'])
-
-print('===================================')
-print(df.dtypes)
-
-df.replace(' ', '_', regex=True, inplace=True)
-print('===================================')
-print(df.head())
-
-X = df.drop('Churn', axis=1).copy()
-print('===================================')
-print(X.head())
-
-y = df['Churn'].copy()
-print('===================================')
-print(y.head())
-
-X_encoded = pd.get_dummies(X, columns= ['gender',
+    df = pd.read_csv(path_to_file)
+    df.drop('customerID', axis=1, inplace=True)
+    df.loc[(df['TotalCharges'] == ' '), 'TotalCharges'] = 0
+    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'])
+    df.replace(' ', '_', regex=True, inplace=True)
+    X = df.drop('Churn', axis=1).copy()
+    y = df['Churn'].copy()
+    X_encoded = pd.get_dummies(X, columns= ['gender',
                                         'SeniorCitizen',
                                         'Partner',
                                         'Dependents',
@@ -59,25 +43,21 @@ X_encoded = pd.get_dummies(X, columns= ['gender',
                                         'PaperlessBilling',
                                         'PaymentMethod'])
 
-print('===================================')
-print(X_encoded.head())
+    y = y.replace({'No': 0, 'Yes': 1})
 
-y = y.replace({'No': 0, 'Yes': 1})
+    X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, stratify=y)
 
-print('===================================')
-print(y.head())
+    return X_train, X_test, y_train, y_test
 
-print('===================================')
-print(sum(y)/len(y))
-
-X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, random_state=42, stratify=y)
+filename = 'data/Telco_customer_churn.csv'
+X_train, X_test, y_train, y_test = preprocess_Telco_customer_churn_data(filename)
 
 param_grid = {
     'max_length': [3, 4, 5],
     'learning_rate': [0.1, 0.2, 0.5, 1],
     'gamma': [0, 0.25, 0.5, 1],
     'reg_gamma': [0, 5.0, 10.0, 15.0, 20.0 ],
-    'scale_pos_weight': [1,3,5]
+    'scale_pos_weight': [1, 3, 5]
 }
 
 
@@ -89,13 +69,13 @@ optimal_params = GridSearchCV(
     scoring= 'roc_auc',
     verbose= 0,
     n_jobs=4,
-    cv= 10
+    cv= 3
 )
 
 optimal_params.fit(
     X_train,
     y_train,
-    early_stopping_rounds= 10,
+    early_stopping_rounds= 15,
     eval_metric= 'auc',
     eval_set= [(X_test, y_test)],
     verbose= False
@@ -106,7 +86,7 @@ print(optimal_params.best_params_)
 
 #Running XGBoost with the bset parameters
 
-clf_xgb = xgb.XGBClassifier(objective='binary:logistic', seed=42,
+clf_xgb = xgb.XGBClassifier(objective='binary:logistic',
                             gamma=optimal_params.best_params_['gamma'],
                             learning_rate= optimal_params.best_params_['learning_rate'],
                             max_depth= optimal_params.best_params_['max_length'],
@@ -117,7 +97,7 @@ clf_xgb = xgb.XGBClassifier(objective='binary:logistic', seed=42,
 
 
 clf_xgb.fit(X_train, y_train, verbose=True,
-            early_stopping_rounds=10,
+            early_stopping_rounds=15,
             eval_metric='aucpr',
             eval_set=[(X_test, y_test)])
 
