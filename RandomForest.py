3d1998415097f29e0c7f3c77f157a89fe72b6965
ildar.askarov.0405@gmail.com <ildar.askarov.0405@gmsil.com>
2020-11-18 15:10:35 +0100

    train a rf model

--- RandomForest.py
+++ RandomForest.py
@@ -9,12 +9,16 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix
 
 from Telco_customer_churn_data import preprocess_Telco_customer_churn_data
 
 filename = 'data/Telco_customer_churn.csv'
 X_train, X_test, y_train, y_test = preprocess_Telco_customer_churn_data(filename)
-print(X_train.shape)
+#print(X_train.shape)
+
 
 param_grid = {
     'bootstrap': [True],
@@ -24,11 +28,34 @@ param_grid = {
     'n_estimators': [200, 400, 700, 1000]
 }
 
-#rf = RandomForestClassifier(random_state= 42)
+rf = RandomForestClassifier(random_state= 42)
 
-#CV_rfc = GridSearchCV(estimator= rf,
-#                      param_grid= param_grid,
-#                      cv = 5,
-#                      )
+optimal_params = GridSearchCV(estimator= rf,
+                      param_grid= param_grid,
+                      cv = 5,
+                      )
 
-#CV_rfc.fit(X_train)
+optimal_params.fit(X_train, y_train)
+
+print("==========  Best Parameters of the model from Grid Search  =============")
+print(optimal_params.best_params_)
+
+
+
+rf_best = RandomForestClassifier(random_state=42,
+                                 max_features= optimal_params.best_params_['max_features'],
+                                 n_estimators= optimal_params.best_params_['n_estimators'],
+                                 max_depth= optimal_params.best_params_['max_depth'],
+                                 min_samples_leaf= optimal_params.best_params_['min_samples_leaf'])
+
+rf_best.fit(X_train, y_train)
+
+prediction = rf_best.predict(X_test)
+print("=======================================")
+print("ROC AUC SCORE for Random Forest on CV data: ", roc_auc_score(y_test,prediction))
+
+print("=======================================")
+plot_confusion_matrix(rf_best, X_test, y_test, values_format='d',
+                      display_labels=['Did not leave', 'Left'])
+
+plt.show()
--- XGBoost.py
+++ XGBoost.py
@@ -14,41 +14,8 @@ import pathlib
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
+from Telco_customer_churn_data import preprocess_Telco_customer_churn_data
 
-def preprocess_Telco_customer_churn_data(filename):
-
-    core_path = os.getcwd()
-    path_to_file = os.path.join(core_path, filename)
-
-    df = pd.read_csv(path_to_file)
-    df.drop('customerID', axis=1, inplace=True)
-    df.loc[(df['TotalCharges'] == ' '), 'TotalCharges'] = 0
-    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'])
-    df.replace(' ', '_', regex=True, inplace=True)
-    X = df.drop('Churn', axis=1).copy()
-    y = df['Churn'].copy()
-    X_encoded = pd.get_dummies(X, columns= ['gender',
-                                        'SeniorCitizen',
-                                        'Partner',
-                                        'Dependents',
-                                        'PhoneService',
-                                        'MultipleLines',
-                                        'InternetService',
-                                        'OnlineSecurity',
-                                        'OnlineBackup',
-                                        'DeviceProtection',
-                                        'TechSupport',
-                                        'StreamingTV',
-                                        'StreamingMovies',
-                                        'Contract',
-                                        'PaperlessBilling',
-                                        'PaymentMethod'])
-
-    y = y.replace({'No': 0, 'Yes': 1})
-
-    X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, stratify=y)
-
-    return X_train, X_test, y_train, y_test
 
 filename = 'data/Telco_customer_churn.csv'
 X_train, X_test, y_train, y_test = preprocess_Telco_customer_churn_data(filename)
